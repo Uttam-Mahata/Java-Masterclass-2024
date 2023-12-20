1afc20840720d9485a16d1071023ce868c9c2993
Uttam_02 <mahatauttam0408@gmail.com>
2023-12-20 16:55:07 +0530

    Exercise Expressions, Methods

---
+++ src/MegaByteConverter.java
@@ -0,0 +1,20 @@
+public class MegaByteConverter {
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(6500);
+    }
+    
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if(kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+        
+        int megaBytes = (kiloBytes/1024);
+        int kB = kiloBytes%1024;
+        
+        
+        System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + kB + " KB" );
+        }
+    }
+
+
+}
