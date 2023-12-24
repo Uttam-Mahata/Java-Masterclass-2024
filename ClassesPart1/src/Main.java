1c6fb15574d869217f181c8de244d62e6febca5d
Uttam_02 <mahatauttam0408@gmail.com>
2023-12-24 07:15:07 +0530

    OOPs

---
+++ ClassesPart1/src/Main.java
@@ -0,0 +1,18 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        Car car = new Car();
+
+        car.setModel("Porsche");
+        System.out.println("make  "+ car.getMake());
+        System.out.println("model  "+ car.getModel());
+        System.out.println("make  "+ car.getMake());
+        car.describeCar();
+
+        }
+
+}
